b9e9a7a74b1376b144aae1ebdb1d79ab06856093
SebaP94 <sebastian.paduch94@gmail.com>
2023-06-02 12:42:51 +0200

    Adjust home page to desktop

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,16 @@
+import { Flex } from "@chakra-ui/react";
 import Logo from "../Logo";
 import NavigationWrapper from "../navigation/NavigationWrapper";
 import styles from "./header.module.css";
 
 export const Header = () => (
-  <header className={styles.container}>
+  <Flex
+    as="header"
+    justifyContent={"space-between"}
+    gap={4}
+    height={"fit-content"}
+  >
     <Logo />
     <NavigationWrapper />
-  </header>
+  </Flex>
 );
