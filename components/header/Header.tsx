a2f024f8b25c65032e78c90023a02cccbcea8881
admahm <admahm@softserveinc.com>
2023-06-21 13:29:39 +0200

    fixed

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex } from "@chakra-ui/react";
 import Link from "next/link";
 
 import { colors } from "../../theme/colors";
@@ -6,22 +6,27 @@ import Logo from "../Logo";
 import NavigationWrapper from "../navigation/NavigationWrapper";
 
 export const Header = () => (
-  <Container
-    backgroundColor={{ base: colors.accent, lg: "transparent" }}
-    maxWidth={"container.xl"}
-  >
-    <Flex
-      as="header"
-      justifyContent={"space-between"}
-      alignItems={"center"}
-      height={{ base: 14, lg: "min-content" }}
-      gap={8}
+  <>
+    <Container
+      backgroundColor={{ base: colors.accent, lg: "transparent" }}
+      maxWidth={"container.xl"}
+      position={"fixed"}
     >
-      <Link href="/">
-        <Logo />
-      </Link>
+      <Flex
+        as="header"
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        height={{ base: 14, lg: "min-content" }}
+        minHeight={{ lg: 48 }}
+        gap={8}
+      >
+        <Link href="/">
+          <Logo />
+        </Link>
 
-      <NavigationWrapper />
-    </Flex>
-  </Container>
+        <NavigationWrapper />
+      </Flex>
+    </Container>
+    <Box height={{ base: 14, lg: "min-content" }} minHeight={{ lg: 48 }}></Box>
+  </>
 );
