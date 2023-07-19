afcb23fd98a72eea3b1ac895fdde49323a54544f
Sp94dev <sp94dev@gmail.com>
2023-07-19 13:24:02 +0200

    feat: Our offer secion

--- theme/components/heading.ts
+++ theme/components/heading.ts
@@ -2,6 +2,9 @@ import { ComponentStyleConfig } from "@chakra-ui/react";
 
 export const CustomHeadingStyle: ComponentStyleConfig = {
   sizes: {
+    md: {
+      fontSize: "1.625rem",
+    },
     xl: {
       fontSize: "2.25rem",
       borderBottom: "1px solid",
@@ -10,5 +13,10 @@ export const CustomHeadingStyle: ComponentStyleConfig = {
       pr: 1,
       pb: 1,
     },
+  },
+  baseStyle: {
+    fontFamily: "Open Sans Condensed",
+    letterSpacing: "4px",
+    fontWeight: 700,
   },
 };
