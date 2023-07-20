d80f0229a1e3a52adeeb4c4b73a781f76c9fe7c5
Sp94dev <sp94dev@gmail.com>
2023-07-20 11:39:08 +0200

    feat: Our actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 
-import { AboutUs } from "../components/about-us/AboutUs";
 import Layout from "../components/Layout";
-import { OurOffer } from "../components/our-offer/OurOffer";
+import { AboutUs } from "../components/sections/AboutUs";
+import { OurActions } from "../components/sections/OurActions";
+import { OurOffer } from "../components/sections/OurOffer";
 
 const IndexPage = () => (
   <Layout>
     <Flex direction="column" gap="32">
       <AboutUs />
       <OurOffer />
+      <OurActions />
     </Flex>
   </Layout>
 );
